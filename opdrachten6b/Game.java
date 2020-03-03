0b56dc25dc09ea3753bb7c34172bf1fc0b9315a3
aegroot <alex.a.groot@student.hu.nl>
2020-03-03 14:38:13 +0100

    init

---
+++ opdrachten6b/Game.java
@@ -0,0 +1,50 @@
+package opdrachten6b;
+
+import java.time.LocalDate;
+
+public class Game{
+    private String naam;
+    private int releasejaar;
+    private double niewprijs;
+
+    public Game(String nm ,int rj,double nwpr){
+        this.naam=nm;
+        this.niewprijs=nwpr;
+        this.releasejaar=rj;
+    }
+    public double huidigewaarde(){
+        int jaar= LocalDate.now().getYear();
+        int jdiff=jaar-releasejaar;
+        if (jdiff==0)
+            return niewprijs;
+        else
+            return niewprijs*(1-Math.pow(0.3,jdiff));
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getReleasejaar() {
+        return releasejaar;
+    }
+    @Override
+    public boolean equals(Object o){
+        boolean gelijk=false;
+       if (o instanceof Game){
+           if (this.niewprijs == ((Game) o).niewprijs&&
+                   this.naam.equals(((Game) o).naam)&&
+                   this.releasejaar==((Game) o).releasejaar) {
+               gelijk=true;
+
+           }
+
+
+       }
+        return gelijk;
+    }
+    @Override
+    public String toString(){
+        return naam+", uitgegeven in "+releasejaar+": niewprijs: "+niewprijs+" nu voor: "+huidigewaarde()+"\n";
+    }
+}
---
+++ opdrachten6b/Persoon.java
@@ -0,0 +1,93 @@
+package opdrachten6b;
+
+import java.util.ArrayList;
+
+
+public class Persoon{
+    private String naam;
+    private double budget;
+    private ArrayList<Game> games;
+
+    public Game zoekGameOpNaam(String nm){
+        for(Game ga:games){
+            if (ga.getNaam()==nm){return ga;}
+
+
+        }
+        return null;
+    }
+
+
+
+
+    public Persoon(String nm,double bud) {
+        this.naam=nm;
+        this.budget=bud;
+        games = new ArrayList<Game>();
+
+    }
+
+    public double getBudget() {
+        return this.budget;
+    }
+    public boolean koop(Game game){
+        if (!(game.huidigewaarde() > budget)||games.contains(game))
+        {games.add(game);
+        budget-=game.huidigewaarde();
+        return true;
+    } else {return false;}
+
+    }
+    public boolean verkoop(Game g, Persoon p){
+        boolean check=false;
+        for(Game ga:games){
+            if (g.equals(ga)){
+                check=true;
+            }
+        }
+
+
+        if ((g.huidigewaarde() > p.budget && check) == false){
+        p.koop(g);
+        games.remove(g);
+        budget+=g.huidigewaarde();
+        return true;}
+        else{
+        return false;}
+
+    }
+
+
+    public ArrayList<Game> bepaalGamesNietInBezit(ArrayList<Game> a) {
+        ArrayList<Game> nietbezit=new ArrayList<Game>();
+
+
+        for(Game g: games){
+            boolean x=false;
+            for (Game b : a){
+                if (b.equals(g)){
+                    x=true;
+                    break;
+
+                }
+
+            }
+            if (x==false){
+                nietbezit.add(g);
+            }
+
+        }
+        return nietbezit;
+    }
+
+
+    @Override
+    public String toString(){
+        String  endString=naam+" heeft een budget van "+budget+" en bezit de volgende games:\n";
+        for(Game game:games){
+            endString+=game.toString();
+        }
+
+        return endString;
+    }
+}
